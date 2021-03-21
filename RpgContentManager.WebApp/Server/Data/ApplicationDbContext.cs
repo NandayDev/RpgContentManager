1e1f9d63997645f5216783f713fe9b7f7e4a64f4
Guido Cardinali <guido.c@spirometry.com>
2021-03-21 15:55:45 +0100

    Added projects

---
+++ RpgContentManager.WebApp/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using RpgContentManager.WebApp.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RpgContentManager.WebApp.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+            EnsureInitialized();
+        }
+
+        #region Initialization
+
+        private static bool _initialized = false;
+
+        private void EnsureInitialized()
+        {
+            if (!_initialized)
+            {
+                _initialized = true;
+                Database.EnsureCreated();
+                Database.Migrate();
+            }
+        }
+
+        #endregion
+    }
+}
